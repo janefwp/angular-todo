9f24e822aa7a550104ea3ea95b72743e472f4ab9
jane.fwp <cuij1012@gmail.com>
2021-07-06 15:41:22 +1000

    add login/register template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component'
 import { AboutComponent } from './about/about.component'
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path:'', component: TodoListComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'login', component:LoginComponent},
+  { path: 'register', component:RegisterComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,18 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [				
+  declarations: [						
     AppComponent,
       TopBarComponent,
       TodoListComponent,
       TodoFormComponent,
-      AboutComponent
+      AboutComponent,
+      LoginComponent,
+      RegisterComponent
    ],
   imports: [
     BrowserModule,
