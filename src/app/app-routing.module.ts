4e288feb18d49f112151f6b5fd8af792cac4ee94
jane.fwp <cuij1012@gmail.com>
2021-07-10 18:39:02 +1000

    delet some noused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component'
 import { AboutComponent } from './about/about.component'
-import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { LoginGuard } from './login.guard';
-import { UserRouteRoutes } from './user/user-route.routing'
 
 const routes: Routes = [
   { path: '', component: TodoListComponent, canActivate: [LoginGuard] },
   { path: 'about', component: AboutComponent },
-  // { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent }
 ];
--- src/app/user/user-route.routing.ts
+++ src/app/user/user-route.routing.ts
@@ -1,11 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { UserComponent } from './user.component';
+
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component'
 import { RegisterComponent } from './register/register.component'
-import { LoginGuard } from '../login.guard';
-import { combineLatest } from 'rxjs';
+
 
 const userroutes: Routes = [
   { path: '', component: LoginComponent},
